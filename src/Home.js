e25e29d7fd0f361bd99d1a4c426f656908df92ad
Lamin Saidy <nimalydias@gmail.com>
2024-12-09 18:04:53 +0000

    Add Click Event Authentication to Home Component

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
-    return (
-      <div className="home">
-        <h2>Homepage</h2>
-      </div>
-    );
+
+  const handleClick = (e) => {
+    console.log('hello bloggers!', e);
   }
-   
-  export default Home;
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
+  }
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click here</button>
+      <button onClick={(e) => handleClickAgain('Alex', e)}>Click here again</button>
+    </div>
+  );
+}
+
+export default Home;
