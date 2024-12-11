3e5f39e89ae39988e12c92dba4a1b476d9961d40
Lamin Saidy <nimalydias@gmail.com>
2024-12-11 16:31:49 +0000

    Add a delete option for users

--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,14 @@ const Home = () => {
     { title: 'Tips on how to get schredded!', body: 'lorem ipsum...', author: 'Isha', id: 3 }
   ])
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!"
-/>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
 }
