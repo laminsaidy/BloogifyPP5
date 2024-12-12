3f00e16fb81703e8e52cca97ab88f02050ee005d
Lamin Saidy <nimalydias@gmail.com>
2024-12-12 12:44:36 +0000

    Create a JSON server file to fecth data

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 
@@ -9,14 +9,22 @@ const Home = () => {
     { title: 'Tips on how to get schredded!', body: 'lorem ipsum...', author: 'Isha', id: 3 }
   ])
 
+  const [name, setName] = useState('Alex');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(name);
+  }, [name])
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('lara')}>change name</button>
     </div>
   );
 }
