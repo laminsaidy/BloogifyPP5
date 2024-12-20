e6e6ff083caec8e4c5b701c6fe725f39f2de8f9e
Lamin Saidy <nimalydias@gmail.com>
2024-12-20 13:19:40 +0000

    Add an AbortController instance

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,10 +6,14 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController(); // AbortController instance
+
     const fetchData = async () => {
       try {
         // Simulate an API call
         setTimeout(() => {
+          if (controller.signal.aborted) return; // Stop if the fetch is aborted
+
           const simulatedBlogs = [
             { title: "My new travel blogs!", body: "lorem ipsum...", author: "Alex", id: 1 },
             { title: "Importance of taking a daily shower!", body: "lorem ipsum...", author: "Tijan", id: 2 },
@@ -19,19 +23,26 @@ const useFetch = (url) => {
           setData(simulatedBlogs);
           setError(null);
         }, 1000); // Simulated delay
-      } catch (error) {
-        setError(error.message);
-        setIsPending(false);
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("Fetch operation canceled");
+        } else {
+          setError(err.message);
+          setIsPending(false);
+        }
       }
     };
 
     fetchData();
+
+    // Cleanup function to cancel the fetch if the component unmounts
+    return () => controller.abort();
   }, [url]);
 
   // Function to delete a blog by id
   const deleteBlog = (id) => {
-    const newBlogs = data.filter((blog) => blog.id !== id);  // Filter out the blog to delete
-    setData(newBlogs);  // Update the state with the new list of blogs
+    const newBlogs = data.filter((blog) => blog.id !== id); // Filter out the blog to delete
+    setData(newBlogs); // Update the state with the new list of blogs
   };
 
   return { data, isPending, error, deleteBlog };
