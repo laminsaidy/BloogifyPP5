21690fdaa944284301afddbbedb2580e9841da9a
LX <132520325+laminsaidy@users.noreply.github.com>
2024-12-27 05:14:29 +0000

    Add some styling to the NavBar

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,44 +1,18 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom"; // Import Link for navigation
+import { Link } from 'react-router-dom';
 
-const BlogList = ({ blogs, title, handleDelete }) => {
-  const [showMenu, setShowMenu] = useState(null); // Tracks which blog menu is open
-
-  const toggleMenu = (id) => {
-    setShowMenu(showMenu === id ? null : id); // Toggle the menu for a specific blog
-  };
-
+const BlogList = ({ blogs }) => {
   return (
     <div className="blog-list">
-      <h2>{title}</h2>
-      {blogs.map((blog) => (
-        <div key={blog.id} className="blog-item">
-          <div className="blog-content">
-            {/* Link added to the blog title */}
-            <Link to={`/blogs/${blog.id}`}>
-              <h3>{blog.title}</h3>
-            </Link>
-            <p>{blog.body}</p>
-            <p><i>By {blog.author}</i></p>
-          </div>
-          <div className="menu">
-            <button onClick={() => toggleMenu(blog.id)}>⋮</button>
-            {showMenu === blog.id && (
-              <div className="dropdown-menu">
-                <button
-                  onMouseEnter={(e) => (e.target.textContent = "Are you sure?")}
-                  onMouseLeave={(e) => (e.target.textContent = "Delete")}
-                  onClick={() => handleDelete(blog.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            )}
-          </div>
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </Link>
         </div>
       ))}
     </div>
   );
-};
-
+}
+ 
 export default BlogList;
